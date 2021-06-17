c781c26f046c6d959c36d029c636057106216cdf
sMARCHz <mart_nattanon@hotmail.com>
2021-06-17 19:54:36 +0700

    add Three number test case02

--- Demo01/src/com/company/MySortTest.java
+++ Demo01/src/com/company/MySortTest.java
@@ -74,4 +74,15 @@ class MySortTest {
         List<Integer> sortedList = mySort.sort();
         assertEquals(expect, sortedList);
     }
+
+    @Test
+    @DisplayName("Sort with [2,1,3]")
+    void sortWithThreeNumberCase02() {
+        List<Integer> input = new ArrayList<>(Arrays.asList(2,1,3));
+        List<Integer> expect = new ArrayList<>(Arrays.asList(1,2,3));
+
+        MySort mySort = new MySort(input);
+        List<Integer> sortedList = mySort.sort();
+        assertEquals(expect, sortedList);
+    }
 }
