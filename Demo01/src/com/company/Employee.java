90598353e0ac3a4f9900f9c2ef3b4c11d108fe03
sMARCHz <mart_nattanon@hotmail.com>
2021-06-17 11:39:42 +0700

    add contructor

--- Demo01/src/com/company/Employee.java
+++ Demo01/src/com/company/Employee.java
@@ -5,6 +5,15 @@ public class Employee {
     private String fname;
     private String lname;
 
+    public Employee() {
+        this("?", "?");
+    }
+
+    public Employee(String fname, String lname) {
+        this.fname = fname;
+        this.lname = lname;
+    }
+
     public String getFullName() {
         return fname + " : " + lname;
     }
