f3554c8e6c2095d9c7074f862b3b75b6fd2e2fdc
sMARCHz <mart_nattanon@hotmail.com>
2021-06-17 12:03:23 +0700

    working with command line

--- Demo01/src/com/company/Main.java
+++ Demo01/src/com/company/Main.java
@@ -4,7 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        Employee employee = new Employee();
+        String fname = args[0];
+        String lname = args[1];
+        Employee employee = new Employee(fname, lname);
         String result = employee.getFullName();
         System.out.println(result);
     }
