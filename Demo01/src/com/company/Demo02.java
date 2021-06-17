5aa739c5a09cb63e08ae0b0ba90b444ff9a8dbb1
sMARCHz <mart_nattanon@hotmail.com>
2021-06-17 14:20:00 +0700

    add demo02 string

---
+++ Demo01/src/com/company/Demo02.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Demo02 {
+    public static void main(String[] args) {
+        String name1 = "March";
+
+        for (int i = 0; i < name1.length(); i++) {
+            System.out.println(name1.charAt(i));
+        }
+
+        int[] scores = new int[]{0, 10, 20, 30 ,40};
+        for (int score : scores) { // For each
+            System.out.println(score);
+        }
+        for (int i = 0; i < scores.length; i++) {
+            System.out.println(i + " : " + scores[i]);
+        }
+        Arrays.stream(scores).forEach(System.out::println);
+    }
+}
