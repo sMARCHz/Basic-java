ade9615064f5343300c1b7591fe97abb1581dc2c
Nattanon Chansamakr <nattanon.ch@kbtg.tech>
2021-06-17 11:24:36 +0700

    add employee

--- Demo01/src/com/company/Employee.java
+++ Demo01/src/com/company/Employee.java
@@ -1,4 +1,11 @@
 package com.company;
 
 public class Employee {
+    private int id;
+    private String fname;
+    private String lname;
+
+    public String getFullName() {
+        return fname + " : " + lname;
+    }
 }
--- Demo01/src/com/company/Main.java
+++ Demo01/src/com/company/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello ...");
+        Employee employee1 = new Employee();
+        String result = employee1.getFullName();
+        System.out.println(result);
     }
 }
