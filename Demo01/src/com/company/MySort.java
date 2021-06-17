f56ce96d738118b130dc0ecc10515a58422c5b51
sMARCHz <mart_nattanon@hotmail.com>
2021-06-17 19:47:19 +0700

    add mySort && empty list case

---
+++ Demo01/src/com/company/MySort.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.List;
+
+public class MySort {
+    private List<Integer> input;
+
+    public MySort(List<Integer> input) {
+        this.input = input;
+    }
+
+    public List<Integer> sort() {
+        if (input.size() == 0) {
+            return input;
+        }
+        return input;
+    }
+}
---
+++ Demo01/src/com/company/MySortTest.java
@@ -0,0 +1,30 @@
+package com.company;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MySortTest {
+    //    Test cases
+//    [] => []
+//    [1] => [1]
+//    [1,2] => [1,2]
+//    [2,1] => [1,2]
+//    [1,2,3] => [1,2,3]
+//    [2,1,3] => [1,2,3]
+//    [1,3,2] => [1,2,3]
+//    [3,2,1] => [1,2,3]
+//    [5,4,3,2,1]  [1,2,3,4,5]
+    @Test
+    void sortWithBlankList() {
+        List<Integer> input = new ArrayList<>();
+        List<Integer> expect = new ArrayList<>();
+
+        MySort mySort = new MySort(input);
+        List<Integer> sortedList = mySort.sort();
+        assertEquals(expect, sortedList);
+    }
+}
