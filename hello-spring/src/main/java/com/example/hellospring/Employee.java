419f5cb1e44fcfa12b34cada1c2fdc8c5f461cc1
sMARCHz <mart_nattanon@hotmail.com>
2021-06-18 12:02:49 +0700

    Working with Object class

---
+++ hello-spring/src/main/java/com/example/hellospring/Employee.java
@@ -0,0 +1,46 @@
+package com.example.hellospring;
+
+import java.util.Objects;
+
+public class Employee {
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return id == employee.id &&
+                Objects.equals(name, employee.name);
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
