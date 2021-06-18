72d39914ae8f9d27dee357b6163bb8f9bc0098a2
sMARCHz <mart_nattanon@hotmail.com>
2021-06-18 16:21:54 +0700

    remove method case

--- hello-spring/src/test/java/com/example/hellospring/workshop01/FizzBuzzTest.java
+++ hello-spring/src/test/java/com/example/hellospring/workshop01/FizzBuzzTest.java
@@ -6,59 +6,6 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class FizzBuzzTest {
-    @Test
-    void checkFizz() {
-        int input = 3;
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        fizzBuzz.setInput(input);
-        boolean isFizz = fizzBuzz.isFizz();
-        assertTrue(isFizz);
-    }
-
-    @Test
-    void checkBuzz() {
-        int input = 5;
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        fizzBuzz.setInput(input);
-        boolean isBuzz = fizzBuzz.isBuzz();
-        assertTrue(isBuzz);
-    }
-
-    @Test
-    void checkFizzAsResult() {
-        int input = 3;
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        fizzBuzz.setInput(input);
-        String actualResult = fizzBuzz.getResult();
-        assertEquals("Fizz", actualResult);
-    }
-
-    @Test
-    void checkBuzzAsResult() {
-        int input = 5;
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        fizzBuzz.setInput(input);
-        String actualResult = fizzBuzz.getResult();
-        assertEquals("Buzz", actualResult);
-    }
-
-    @Test
-    void checkFizzBuzzAsResult() {
-        int input = 15;
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        fizzBuzz.setInput(input);
-        String actualResult = fizzBuzz.getResult();
-        assertEquals("FizzBuzz", actualResult);
-    }
-
-    @Test
-    void checkNoFizzBuzzAsResult() {
-        int input = 4;
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        fizzBuzz.setInput(input);
-        String actualResult = fizzBuzz.getResult();
-        assertEquals("4", actualResult);
-    }
 
     @Test
     @DisplayName("1 -> 1")
