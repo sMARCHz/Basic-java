e641381b2f1cc5f51497819a8be7a8a33394b778
sMARCHz <mart_nattanon@hotmail.com>
2021-06-20 21:36:46 +0700

    create Player class

---
+++ hello-spring/src/main/java/com/example/hellospring/refactor/Player.java
@@ -0,0 +1,23 @@
+package com.example.hellospring.refactor;
+
+public class Player {
+    private int score = 0;
+    private String name = "";
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void wonPoint() {
+        this.score += 1;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
--- hello-spring/src/main/java/com/example/hellospring/refactor/TennisGame3.java
+++ hello-spring/src/main/java/com/example/hellospring/refactor/TennisGame3.java
@@ -1,17 +1,17 @@
 package com.example.hellospring.refactor;
 
 public class TennisGame3 {
-    private int player1Score;
-    private int player2Score;
-    private String player1Name;
-    private String player2Name;
+    private Player player1;
+    private Player player2;
 
     public TennisGame3(String player1Name, String player2Name) {
-        this.player1Name = player1Name;
-        this.player2Name = player2Name;
+        this.player1 = new Player(player1Name);
+        this.player2 = new Player(player2Name);
     }
 
     public String getScore() {
+        int player1Score = player1.getScore();
+        int player2Score = player2.getScore();
         if (player1Score < 4 && player2Score < 4 && !(player1Score + player2Score == 6)) {
             String[] callList = new String[]{"Love", "Fifteen", "Thirty", "Forty"};
             String player1Call = callList[player1Score];
@@ -20,16 +20,15 @@ public class TennisGame3 {
         } else {
             if (player1Score == player2Score)
                 return "Deuce";
-            String leadingPlayer = player1Score > player2Score ? player1Name : player2Name;
+            String leadingPlayer = player1Score > player2Score ? player1.getName() : player2.getName();
             return ((player1Score-player2Score)*(player1Score-player2Score) == 1) ? "Advantage " + leadingPlayer : "Win for " + leadingPlayer;
         }
     }
 
     public void wonPoint(String playerName) {
         if (playerName == "player1")
-            this.player1Score += 1;
+            this.player1.wonPoint();
         else
-            this.player2Score += 1;
-
+            this.player2.wonPoint();
     }
 }
