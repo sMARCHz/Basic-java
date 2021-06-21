b874a7b6763ccd925fe8b9ed6ef02e48b32a2f28
sMARCHz <mart_nattanon@hotmail.com>
2021-06-21 14:05:43 +0700

    add request param & test case

--- hello-rest/src/main/java/com/example/hellorest/employee/EmployeeController.java
+++ hello-rest/src/main/java/com/example/hellorest/employee/EmployeeController.java
@@ -2,12 +2,25 @@ package com.example.hellorest.employee;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EmployeeController {
     @GetMapping("/employee/{id}")
     public EmployeeResponse getEmployeeByID(@PathVariable String id) {
+        int _id = 0;
+        try{
+            _id = Integer.parseInt(id);
+        }
+        catch (NumberFormatException err) {
+            System.out.println("Cannot convert to number");
+        }
+        return new EmployeeResponse(_id, "Nattanon", "Ch");
+    }
+
+    @GetMapping("/employee")
+    public EmployeeResponse getEmployeeByID2(@RequestParam String id) {
         int _id = 0;
         try{
             _id = Integer.parseInt(id);
---
+++ hello-rest/src/test/java/com/example/hellorest/employee/EmployeeControllerTest.java
@@ -0,0 +1,16 @@
+package com.example.hellorest.employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class EmployeeControllerTest {
+
+    @Test
+    public void callApiWithPathVariable() {
+
+    }
+}
