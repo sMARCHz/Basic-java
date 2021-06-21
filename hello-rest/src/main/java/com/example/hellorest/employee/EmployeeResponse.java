5a45c91d2720d76da66bc6193933451395707624
sMARCHz <mart_nattanon@hotmail.com>
2021-06-21 14:34:33 +0700

    add compare object test

--- hello-rest/src/main/java/com/example/hellorest/employee/EmployeeResponse.java
+++ hello-rest/src/main/java/com/example/hellorest/employee/EmployeeResponse.java
@@ -1,15 +1,30 @@
 package com.example.hellorest.employee;
 
+import java.util.Objects;
+
 public class EmployeeResponse {
 
     private int id;
     private String fname;
     private String lname;
 
+    public EmployeeResponse() {
+    }
+
     public EmployeeResponse(int id, String fname, String lname) {
         this.id = id;
         this.fname = fname;
         this.lname = lname;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        EmployeeResponse that = (EmployeeResponse) o;
+        return id == that.id &&
+                Objects.equals(fname, that.fname) &&
+                Objects.equals(lname, that.lname);
     }
 
     public int getId() {
