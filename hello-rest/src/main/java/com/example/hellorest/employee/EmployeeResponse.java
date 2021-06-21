6a578b6669f1049314feae2f197048cb44d40936
sMARCHz <mart_nattanon@hotmail.com>
2021-06-21 11:53:32 +0700

    rearrange variable

--- hello-rest/src/main/java/com/example/hellorest/employee/EmployeeResponse.java
+++ hello-rest/src/main/java/com/example/hellorest/employee/EmployeeResponse.java
@@ -2,22 +2,14 @@ package com.example.hellorest.employee;
 
 public class EmployeeResponse {
 
-    private String fname;
     private int id;
+    private String fname;
     private String lname;
 
     public EmployeeResponse(int id, String fname, String lname) {
-        this.fname = fname;
         this.id = id;
-        this.lname = lname;
-    }
-
-    public String getFname() {
-        return fname;
-    }
-    
-    public void setFname(String fname) {
         this.fname = fname;
+        this.lname = lname;
     }
 
     public int getId() {
@@ -26,6 +18,14 @@ public class EmployeeResponse {
 
     public void setId(int id) {
         this.id = id;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
     }
 
     public String getLname() {
