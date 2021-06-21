2b717f56f7b2d7d0d4de80b821109c7b93a55590
sMARCHz <mart_nattanon@hotmail.com>
2021-06-21 14:45:03 +0700

    add bean count

--- hello-rest/src/main/java/com/example/hellorest/HelloRestApplication.java
+++ hello-rest/src/main/java/com/example/hellorest/HelloRestApplication.java
@@ -2,11 +2,14 @@ package com.example.hellorest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class HelloRestApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HelloRestApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(HelloRestApplication.class, args);
+		int count = context.getBeanDefinitionCount();
+		System.out.println(count);
 	}
 }
