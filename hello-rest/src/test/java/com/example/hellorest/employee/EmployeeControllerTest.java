8cca5a0ace4fdb78d851dea427ed33ad03d37e80
sMARCHz <mart_nattanon@hotmail.com>
2021-06-21 14:14:00 +0700

    add test

--- hello-rest/src/test/java/com/example/hellorest/employee/EmployeeControllerTest.java
+++ hello-rest/src/test/java/com/example/hellorest/employee/EmployeeControllerTest.java
@@ -1,16 +1,34 @@
 package com.example.hellorest.employee;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class EmployeeControllerTest {
 
+    @Autowired
+    private TestRestTemplate restTemplate;
+
     @Test
     public void callApiWithPathVariable() {
+        EmployeeResponse response
+                = restTemplate.getForObject("/employee/123", EmployeeResponse.class);
+        assertEquals(123, response.getId());
+        assertEquals("Nattanon", response.getFname());
+        assertEquals("Ch", response.getLname());
+    }
 
+    @Test
+    public void callApiWithREquestParameter() {
+        EmployeeResponse response
+                = restTemplate.getForObject("/employee?id=999", EmployeeResponse.class);
+        assertEquals(999, response.getId());
+        assertEquals("Nattanon", response.getFname());
+        assertEquals("Ch", response.getLname());
     }
 }
