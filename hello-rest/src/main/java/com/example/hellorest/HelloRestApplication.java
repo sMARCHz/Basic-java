34f4bd99358094f5e4ae15e83058e21ebd7552a0
sMARCHz <mart_nattanon@hotmail.com>
2021-06-21 16:56:57 +0700

    Work with commandLineRunner in Spring boot project

--- hello-rest/src/main/java/com/example/hellorest/HelloRestApplication.java
+++ hello-rest/src/main/java/com/example/hellorest/HelloRestApplication.java
@@ -1,12 +1,13 @@
 package com.example.hellorest;
 
 import com.example.hellorest.employee.Demo;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
-public class HelloRestApplication {
+public class HelloRestApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context = SpringApplication.run(HelloRestApplication.class, args);
@@ -22,5 +23,11 @@ public class HelloRestApplication {
 		Demo demo2 = context.getBean(Demo.class);
 		System.out.println("Demo1 => " + demo1.getName());
 		System.out.println("Demo2 => " + demo2.getName());
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Start...");
+		System.out.println("Stop...");
 	}
 }
