33e3e180b75e3b60fd9c982201e89c2183994420
sMARCHz <mart_nattanon@hotmail.com>
2021-06-21 14:55:55 +0700

    print bean name

--- hello-rest/src/main/java/com/example/hellorest/HelloRestApplication.java
+++ hello-rest/src/main/java/com/example/hellorest/HelloRestApplication.java
@@ -9,7 +9,11 @@ public class HelloRestApplication {
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context = SpringApplication.run(HelloRestApplication.class, args);
+		String[] objects = context.getBeanDefinitionNames();
+		for (String object : objects) {
+			System.out.println(object);
+		}
 		int count = context.getBeanDefinitionCount();
-		System.out.println(count);
+		System.out.println(count); // 197
 	}
 }
