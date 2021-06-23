b732876363f815d6739a5601a359cd38c9df0d71
sMARCHz <mart_nattanon@hotmail.com>
2021-06-23 11:47:23 +0700

    Controller testing with mock database

---
+++ hello-rest/src/test/java/com/example/hellorest/employee/EmployeeMockBeanTest.java
@@ -0,0 +1,67 @@
+package com.example.hellorest.employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.when;
+import static org.springframework.boot.test.context.SpringBootTest.*;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class EmployeeMockBeanTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @MockBean
+    private MyRandom random;
+
+    @MockBean
+    private EmployeeRepository repository;
+
+    @Test
+    public void callApiWithPathVariableWithoutDatabase() {
+        // mock
+        when(random.nextInt(anyInt())).thenReturn(5);
+        Employee mock = new Employee(123, "Mock fname", "Mock lname");
+        when(repository.findById(123)).thenReturn(
+                Optional.of(mock)
+        );
+
+        EmployeeResponse expected
+                = new EmployeeResponse(123, "Mock fname5", "Mock lname");
+        EmployeeResponse response
+                = restTemplate.getForObject("/employee/123", EmployeeResponse.class);
+
+        assertEquals(123, response.getId());
+        assertEquals("Mock fname5", response.getFname());
+        assertEquals("Mock lname", response.getLname());
+        assertEquals(expected, response);
+    }
+
+    @Test
+    public void callApiWithPathVariableWithoutDatabaseAndEmployeeNotFound() {
+        // mock
+        when(random.nextInt(anyInt())).thenReturn(5);
+        when(repository.findById(123)).thenReturn(
+                Optional.empty()
+        );
+
+        EmployeeResponse expected
+                = new EmployeeResponse();
+        EmployeeResponse response
+                = restTemplate.getForObject("/employee/123", EmployeeResponse.class);
+
+        assertEquals(0, response.getId());
+        assertNull(response.getFname());
+        assertNull(response.getLname());
+        assertEquals(expected, response);
+    }
+}
