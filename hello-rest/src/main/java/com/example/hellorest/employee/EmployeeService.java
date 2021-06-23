84983b8b34f1d76d4d738ee48c56dc97de9f35e2
sMARCHz <mart_nattanon@hotmail.com>
2021-06-23 13:24:52 +0700

    fix Service

--- hello-rest/src/main/java/com/example/hellorest/employee/EmployeeService.java
+++ hello-rest/src/main/java/com/example/hellorest/employee/EmployeeService.java
@@ -1,9 +1,11 @@
 package com.example.hellorest.employee;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+@Service
 public class EmployeeService {
 
     @Autowired
