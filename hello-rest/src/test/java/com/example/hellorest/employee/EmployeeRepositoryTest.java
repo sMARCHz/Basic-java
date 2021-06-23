4a1639e24f5ae52fa177669fe714fca7b632ff8b
sMARCHz <mart_nattanon@hotmail.com>
2021-06-23 10:06:15 +0700

    create EmployeeRepo test

---
+++ hello-rest/src/test/java/com/example/hellorest/employee/EmployeeRepositoryTest.java
@@ -0,0 +1,22 @@
+package com.example.hellorest.employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+public class EmployeeRepositoryTest {
+
+    @Autowired
+    private EmployeeRepository repository;
+
+    @Test
+    public void foundWithID1() {
+        Employee employee1 = repository.getById(1);
+        assertEquals(1, employee1.getId());
+        assertEquals("Nattanon from DB", employee1.getFirstName());
+        assertEquals("Ch from DB", employee1.getLastName());
+    }
+}
